1e1dfc801237dfa8c764006b684a55a11847869e
Joe <mlt@mlt.gov.ua>
2025-07-12 16:59:39 +0300

    feat: Update QMS Service Point options and modify CSS padding

--- qms_cherga/qms_cherga/doctype/qms_service_point/qms_service_point.py
+++ qms_cherga/qms_cherga/doctype/qms_service_point/qms_service_point.py
@@ -6,4 +6,20 @@ from frappe.model.document import Document
 
 
 class QMSServicePoint(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		assigned_operator: DF.Link | None
+		description: DF.SmallText | None
+		ip_address: DF.Data | None
+		is_active: DF.Check
+		office: DF.Link | None
+		point_name: DF.Data
+	# end: auto-generated types
+
 	pass
